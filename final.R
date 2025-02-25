0f5c6fd6e7357387f3c46498875f4acc97e30583
epdumont <epdumont@ucdavis.edu>
2025-02-24 18:15:29 -0800

    Add files via upload

---
+++ final.R
@@ -0,0 +1,7 @@
+# Install the released version from CRAN:
+install.packages("agridat")
+
+# Install the development version from GitHub:
+install.packages("devtools")
+devtools::install_github("kwstat/agridat")
+library(agridat)
